dd38c00abd748fd8381063cc8c5f05f73fd6b255
GianCordova <gcordova-2024016@kinal.edu.gt>
2026-03-02 11:42:57 -0600

    Se completo la parte del Jwt

--- src/AuthService.Application/DTOs/Email/ForgotPasswordDto.cs
+++ src/AuthService.Application/DTOs/Email/ForgotPasswordDto.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AuthService.Application.DTOs.Email;
 
-public class ForgtotPasswordDto
+public class ForgotPasswordDto
 {
     [Required(ErrorMessage = "El email es obligatorio.")]
     [EmailAddress(ErrorMessage = "El email no es válido.")]
